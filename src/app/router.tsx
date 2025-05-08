bae38f3bc42da6849aa9fb5ae864270fa4bcd0f2
BusanGukbap <dong2857210@gmail.com>
2025-05-08 12:43:56 +0900

    ⚡️fix : 로그인 페이지와 회원가입 페이지 라우터에 추가

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Splash from '../pages/Splash'
-import Login from '../pages/Login'
-
+import LoginSelect from '../pages/LoginSelect'
+import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SignUp'
 export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Splash />,
-  },
-  {
-    path: '/login',
-    element: <Login />,
-  },
+  { path: '/', element: <Splash /> },
+  { path: '/login', element: <LoginSelect /> },
+  { path: '/signin', element: <SignIn /> },
+  { path: '/signup', element: <SignUp /> }
 ])
