d1273e6d3c8e51b131636e2b21f11eb60dad6155
BusanGukbap <dong2857210@gmail.com>
2025-05-09 17:22:14 +0900

    :recycle: refactoring : Footer 컴포넌트 구현

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+// Footer.tsx
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="mt-12 text-center text-[#F15A24] font-bold">
+      <span className="bg-[#F15A24] text-white rounded-full w-3 h-3 inline-block mr-2" />
+      reciping.
+      <p className="text-xs font-normal text-[#F15A24] mt-1">AI 기반 통합 레시피 검색 플랫폼</p>
+    </div>
+  );
+};
+
+export default Footer;
