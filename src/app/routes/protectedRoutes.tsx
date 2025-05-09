4a09bc275fe4db12eff8a456d7a431a718056351
BusanGukbap <dong2857210@gmail.com>
2025-05-09 10:48:58 +0900

    ✨ feat : 프로필 페이지 라우터 구분
    토큰 없으면 login페이지로
    토큰 있으면 프로필 페이지로

--- src/app/routes/protectedRoutes.tsx
+++ src/app/routes/protectedRoutes.tsx
@@ -1,8 +1,15 @@
 // src/app/routes/protectedRoutes.tsx
 import Write from '../../pages/Write'
+import Profile from '../../pages/Profile'
 import { Navigate } from 'react-router-dom'
+
 // ✅ 로그인 여부를 렌더링 시점에 판단하는 컴포넌트
 const UnauthenticatedWrite = () => {
+  alert('로그인 후 이용해주세요.')
+  return <Navigate to="/login" />
+}
+
+const UnauthenticatedProfile = () => {
   alert('로그인 후 이용해주세요.')
   return <Navigate to="/login" />
 }
@@ -13,6 +20,13 @@ export const protectedRoutes = [
     element: (() => {
       const token = localStorage.getItem('token')
       return token ? <Write /> : <UnauthenticatedWrite />
+    })()
+  },
+  {
+    path: '/profile',
+    element: (() => {
+      const token = localStorage.getItem('token')
+      return token ? <Profile /> : <UnauthenticatedProfile />
     })()
   }
 ]
