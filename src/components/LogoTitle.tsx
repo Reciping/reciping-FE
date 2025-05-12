91052d59c84e6d96f975b12c5c24aba421a2b2f9
BusanGukbap <dong2857210@gmail.com>
2025-05-12 10:22:49 +0900

    ✨ feat : 타이틀 클릭하면 Home으로 이동

--- src/components/LogoTitle.tsx
+++ src/components/LogoTitle.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const LogoTitle = () => {
   return (
-    <div className="text-6xl font-bold text-[#F15A24] mb-4">
-      {/* 원래 span + 텍스트 */}
+    <Link
+      to="/home"
+      className="inline-block text-6xl font-bold text-[#F15A24] mb-4"
+    >
+      {/* 기존 span + 텍스트 */}
       <span className="bg-[#F15A24] text-white rounded-full w-4 h-4 inline-block mr-2" />
       reciping.
-    </div>
+    </Link>
   )
 }
 
