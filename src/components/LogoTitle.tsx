7edf9700522d85dc087c937c24ab383aa17bd3bc
BusanGukbap <dong2857210@gmail.com>
2025-05-12 10:41:12 +0900

    💄 design : 타이틀 중앙으로 이동

--- src/components/LogoTitle.tsx
+++ src/components/LogoTitle.tsx
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom'
 
 const LogoTitle = () => {
   return (
-    <Link
-      to="/home"
-      className="inline-block text-6xl font-bold text-[#F15A24] mb-4"
-    >
-      {/* 기존 span + 텍스트 */}
-      <span className="bg-[#F15A24] text-white rounded-full w-4 h-4 inline-block mr-2" />
-      reciping.
-    </Link>
+    <div className = "w-full flex justify-center mb-4">
+      <Link
+        to="/home"
+        className="text-6xl font-bold text-[#F15A24] flex items-center"
+      >
+        {/* 기존 span + 텍스트 */}
+        <span className="bg-[#F15A24] text-white rounded-full w-4 h-4 inline-block mr-2" />
+        reciping.
+      </Link>
+    </div>
+
   )
 }
 
