143f0762d7a28e08ebbb6844412c961f24edf391
BusanGukbap <dong2857210@gmail.com>
2025-05-16 00:24:03 +0900

    ♻️ refactoring : Mypage -> Profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import PageLayout from '../../components/layout/PageLayout';
 import ContentWrapper from '../../components/common/ContentWrapper';
 
-import { getMyPage } from '../../api/mypage';
+import { getMyPage } from '../../api/profile';
 import type { MyPageData } from '../../types/mypage';
 
 type Tab = 'myRecipes' | 'myBookmarks';
