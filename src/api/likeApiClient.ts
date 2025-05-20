2f81f788c116e8a504f9cc63fe5dbedeb0e5b233
BusanGukbap <dong2857210@gmail.com>
2025-05-20 14:27:21 +0900

    :truck: fix : 함수명 변경

--- src/api/likeApiClient.ts
+++ src/api/likeApiClient.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const authApiClient = axios.create({
+export const likeApiClient = axios.create({
     baseURL: import.meta.env.VITE_API_LIKE_BASE || 'http://localhost:8080',
     withCredentials: true,
     timeout: 10_000,
